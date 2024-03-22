0c8176411a6e3d933cdf2618d39a43107501ebe2
guilhermepereiradev <guilhermepereira1609@hotmail.com>
2024-03-21 21:20:56 -0300

    first commit

---
+++ src/main/java/com/grupo3/fisiowave/FisiowaveApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.fisiowave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisiowaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisiowaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo3/fisiowave/FisiowaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.fisiowave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisiowaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
